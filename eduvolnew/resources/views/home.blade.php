7e24dacd887800071ec74f172528e883f7c71dbe
sitinrbyni1 <nurbayani0025@gmail.com>
2025-04-29 23:59:46 +0700

    commit codingan setelah error

---
+++ eduvolnew/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <h1 class="text-center mt-5">Selamat Datang di EDU Volunteer</h1>
+            <p class="text-center">Platform untuk menghubungkan relawan pendidikan dengan mereka yang membutuhkan.</p>
+        </div>
+    </div>
+</div>
+@endsection 
